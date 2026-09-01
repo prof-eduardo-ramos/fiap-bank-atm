2db71bafc0e82420d0a6c1c0f23073442e76c79e
eduardo-ramos-ntconsult <eduardo.ramos@ntconsult.com.br>
2026-09-01 18:24:00 -0300

    refactor: update AccountRepository type declaration to use interface in AtmApplication

--- src/main/java/com/fiap/bank/atm/AtmApplication.java
+++ src/main/java/com/fiap/bank/atm/AtmApplication.java
@@ -1,6 +1,7 @@
 package com.fiap.bank.atm;
 
 import com.fiap.bank.atm.application.service.AtmService;
+import com.fiap.bank.atm.domain.repository.AccountRepository;
 import com.fiap.bank.atm.infrastructure.persistence.InMemoryAccountRepository;
 import com.fiap.bank.atm.presentation.AtmFrame;
 import javax.swing.SwingUtilities;
@@ -8,10 +9,11 @@ import javax.swing.SwingUtilities;
 public class AtmApplication {
     public static void main(String[] args) {
         // Inicializa as camadas de Infraestrutura e Aplicação (DDD)
-        InMemoryAccountRepository accountRepository = new InMemoryAccountRepository();
+        AccountRepository accountRepository = new InMemoryAccountRepository();
         AtmService atmService = new AtmService(accountRepository);
 
-        // Inicializa a camada de Apresentação de forma segura na Event Dispatch Thread (EDT)
+        // Inicializa a camada de Apresentação de forma segura na Event Dispatch Thread
+        // (EDT)
         SwingUtilities.invokeLater(() -> {
             AtmFrame mainFrame = new AtmFrame(atmService);
             mainFrame.setVisible(true);
