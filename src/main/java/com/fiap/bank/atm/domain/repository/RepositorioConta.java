27a0fa0caa43891a55dd6a68ccbb843bd1c0f4c7
eduardo-ramos-ntconsult <eduardo.ramos@ntconsult.com.br>
2026-09-01 15:43:39 -0300

    refactor: remove ATM service, repository layer, and presentation files

--- src/main/java/com/fiap/bank/atm/domain/repository/RepositorioConta.java
+++
@@ -1,9 +0,0 @@
-package com.fiap.bank.atm.domain.repository;
-
-import com.fiap.bank.atm.domain.model.Conta;
-import java.util.Optional;
-
-public interface RepositorioConta {
-    Optional<Conta> buscarPorNumero(String numeroConta);
-    void salvar(Conta conta);
-}
